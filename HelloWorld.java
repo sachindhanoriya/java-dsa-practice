1cdfc1b4b752bd42f3157fcd5f93a05ec9291505
Sachin Dhanoriya <sachindhanoriya@outlool.com>
2026-02-25 10:09:07 +0530

    Misc changes

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,12 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        var x = 1;
+        System.out.println(x instanceof int);
+        MyRecord rec = new MyRecord(5);
+        System.out.println(rec);
+        System.out.println(rec.y());
     }
 }
+
+record MyRecord(int y) {}
