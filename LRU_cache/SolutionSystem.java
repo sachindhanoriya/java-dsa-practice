7bfea4b9bece4109c9bbaf91a855a251ba8e7d7d
Sachin Dhanoriya <sachindhanoriya@outlool.com>
2026-04-27 10:35:31 +0530

    LRU Cache Driver Method Fixed to have Generics

--- LRU_cache/SolutionSystem.java
+++ LRU_cache/SolutionSystem.java
@@ -2,7 +2,7 @@ package LRU_cache;
 
 public class SolutionSystem {
     public static void main(String[] args) {
-        LRUCacheWithBuiltInDS cache = new LRUCacheWithBuiltInDS(4);
+        LRUCacheWithBuiltInDS<Integer, Integer> cache = new LRUCacheWithBuiltInDS<>(4);
         cache.put(1,2);
         System.out.println(cache.get(1));
     }
