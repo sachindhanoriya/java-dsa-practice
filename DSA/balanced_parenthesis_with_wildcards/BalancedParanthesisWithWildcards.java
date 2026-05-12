dce0c88889b9584eb4584704cc02807aaecaa264
Sachin Dhanoriya <sachindhanoriya@outlool.com>
2026-05-12 12:30:30 +0530

    BalancedParenthesisWithWildcards: Using DP-Memoization

--- DSA/balanced_parenthesis_with_wildcards/BalancedParanthesisWithWildcards.java
+++ DSA/balanced_parenthesis_with_wildcards/BalancedParanthesisWithWildcards.java
@@ -1,57 +1,55 @@
 package DSA.balanced_parenthesis_with_wildcards;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
 
 public class BalancedParanthesisWithWildcards {
     public static void main(String[] args) {
         Solution sol = new Solution();
-        String[] testCases = new String[]{"()", "(*)", "((*)", "(*))", "*(", "*)", ")*", "(*))*", "(*)*((**", "**))"};
+        String[] testCases = new String[]{"***((*", "****", "***)))", "()", "(*)", "((*)", "(*))", "*(", "*)", ")*", "(*))*", "(*)*((**", "**))"};
         for (int i = 0; i < testCases.length; i++) {
             System.out.println(sol.checkValidString(testCases[i]));
         }
     }
 }
 
-/*
- ( ( * )
- 1 2 2 2
- 1 1 1 1
-
- ( * ) )
- 1 1 1 1
- 2 2 2 1
-
- ( ( ( * * ) ( * * ) )
- 1 2 3 3 3 2 3 3 3 2 1
--1 0 1 2 2 2 1 2 2 2 1
-*/
 
 class Solution {
     private String str;
     private int strLen;
+    private int[][] cache;
+
     public boolean checkValidString(String s) {
         this.str = s;
         this.strLen = s.length();
-        return recurse(0, 0);
+        this.cache = new int[strLen + 1][strLen + 1];
+        for (int[] row: cache) 
+            Arrays.fill(cache, -1);
+        return recurseDPMemo(0, 0);
     }
 
-    private boolean recurse(int curIdx, int openBraces) {
+    private boolean recurseDPMemo(int curIdx, int openBraces) {
         if (curIdx == strLen)
             return openBraces == 0;
+
+        if (this.cache[curIdx][openBraces] > -1)
+            return cache[curIdx][openBraces] == 1;
         
         char curChar = this.str.charAt(curIdx);
         boolean isValid = false;
         
         if (curChar == '(') {
-            isValid |= recurse(curIdx + 1, openBraces + 1);
-        }
-        else if (curChar == '*') {
-            isValid |= recurse(curIdx + 1, openBraces + 1);
+            isValid |= recurseDPMemo(curIdx + 1, openBraces + 1);
+        } else if (curChar == '*') {
+            isValid |= recurseDPMemo(curIdx + 1, openBraces + 1);
             if (openBraces > 0)
-                isValid |= recurse(curIdx + 1, openBraces - 1);
-            isValid |= recurse(curIdx + 1, openBraces);
+                isValid |= recurseDPMemo(curIdx + 1, openBraces - 1);
+            isValid |= recurseDPMemo(curIdx + 1, openBraces);
         } else if (openBraces > 0) {
-            isValid |= recurse(curIdx + 1, openBraces - 1);
+            isValid |= recurseDPMemo(curIdx + 1, openBraces - 1);
         }
+        
+        cache[curIdx][openBraces] = isValid ? 1: 0;
         return isValid;
     }
 }
